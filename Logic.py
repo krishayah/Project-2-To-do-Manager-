50f0dc6e2dffbc8e9d067315b82b10dfa6654df7
krishayah <krishagurung26@gmail.com>
2024-12-03 15:50:56 -0600

    functions overview

---
+++ Logic.py
@@ -0,0 +1,40 @@
+class Logic:
+    def __init__(self):
+        pass
+
+    def add_task(self):
+        pass
+
+    def delete_task(self):
+        pass
+
+    def mark_completed(self):
+        pass
+
+    def edit_task(self):
+        pass
+
+    def get_all_tasks(self):
+        pass
+
+    def search_tasks(self):
+        pass
+
+
+    """FOR STREAKS"""
+    def track_streak(self):
+        pass
+
+    def update_streak(self):
+        pass
+
+    """UTILITY FUNCTIONS"""
+    def validate_input(self):
+        pass
+
+    def save_to_file(self):
+        pass
+
+    def load_tasks_4m_file(self):
+        pass
+
